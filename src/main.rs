e45c15255f7baacc8957c9042758ddd2dd18517b
raimon <raimon49@hotmail.com>
2020-09-28 17:51:33 +0900

    Add a code about define struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// 8bitグレースケールピクセルの長方形
+// 構造体を含む型の名前はCamelCaseを使う慣習
+struct GrayscaleMap {
+    // フィールド名/メソッド名はsnake_case
+    pixels: Vec<u8>,
+    size: (usize, usize)
+}
+
+fn new_map(size: (usize, usize), pixels: Vec<u8>) -> GrayscaleMap {
+    assert_eq!(pixels.len(), size.0 * size.1);
+    GrayscaleMap { pixels, size }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let width = 1024;
+    let height = 576;
+    let image = new_map((width, height), vec![0; width * height]);
+
+    assert_eq!(image.size, (1024, 576));
+    assert_eq!(image.pixels.len(), 1024 * 576);
 }
