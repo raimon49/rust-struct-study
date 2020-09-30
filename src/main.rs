bae3cdcd3f4d2f4d33f516c49973f19d2b8a832c
raimon <raimon49@hotmail.com>
2020-09-30 20:32:05 +0900

    Define function 'chop'

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,15 @@ struct Broom {
 #[derive(Copy, Clone)]
 enum BroomIntent { FetchWater, DumpWater } // Broomができる2つの活動
 
+fn chop(b: Broom) -> (Broom, Broom) {
+    let mut broom1 = Broom { height: b.height / 2, .. b };
+    let mut broom2 = Broom { name: broom1.name.clone(), .. broom1 };
+    broom1.name.push_str(" I");
+    broom2.name.push_str(" II");
+
+    (broom1, broom2)
+}
+
 fn main() {
     let width = 1024;
     let height = 576;
