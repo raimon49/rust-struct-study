8696680e48ea33762860b005c973041c99602d36
raimon <raimon49@hotmail.com>
2020-10-08 18:54:46 +0900

    Implement method 'pop'

--- src/main.rs
+++ src/main.rs
@@ -58,6 +58,20 @@ impl Queue {
     pub fn push(&mut self, c: char) {
         self.younger.push(c);
     }
+
+    pub fn pop(&mut self) -> Option<char> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+
+            use std::mem::swap;
+            swap(&mut self.older, &mut self.younger);
+            self.older.reverse();
+        }
+
+        self.older.pop()
+    }
 }
 
 fn main() {
